0a3ff231e9fb0d1296d290ca06ac5b11496a5c3b
TNlap <phnt0604@gmail.com>
2024-05-11 13:52:08 +0700

    [thuy] noi dung thay doi

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link, useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { StoreContext } from '../../context/StoreContext'
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
 
